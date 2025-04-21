37c97f6ee1cfc8d6975620c91c8aafdd40939250
ikerjik <kerj4k@yandex.ru>
2025-04-21 10:30:08 +0700

    Added camera for stage that can zoom and move

---
+++ src/Camera.ts
@@ -0,0 +1,95 @@
+class Camera {
+
+    stage: Stage;
+
+    changes: CameraChanges;
+
+    private _x: number;
+    private _y: number;
+    private _direction: number = 0;
+    private _renderRadius: number;
+    private _zoom = 1;
+
+    constructor(stage:Stage) {
+        this.stage = stage;
+        this._x = this.stage.width / 2;
+        this._y = this.stage.height / 2;
+        this.updateRenderRadius();
+        this.changes = new CameraChanges();
+    }
+
+    set direction(value){
+        let direction = value % 360;
+        direction = direction < 0 ? direction + 360 : direction;
+
+        this.changes.direction = direction - this._direction;
+
+        this._direction = direction;
+    }
+
+    get direction(){
+        return this._direction;
+    }
+
+    get angleDirection() {
+        return this._direction * Math.PI / 180;
+    }
+
+    get width(){
+        return this.stage.width / this._zoom;
+    }
+
+    get height(){
+        return this.stage.height / this._zoom;
+    }
+
+    set x(value){
+        this.changes.x = value - this._x;
+
+        this._x = value;
+    }
+
+    get x(){
+        return this._x;
+    }
+
+    set y(value){
+        this.changes.y = value - this._y;
+
+        this._y = value;
+    }
+
+    get y(){
+        return this._y;
+    }
+
+    get startCornerX (): number {
+        return this._x - this.stage.width / 2;
+    }
+
+    get startCornerY (): number {
+        return this._y - this.stage.height / 2;
+    }
+
+    get renderRadius() {
+        return this._renderRadius;
+    }
+
+    set zoom(value){
+        const newZoom = value < 0.1 ? 0.1 : value
+
+        this.changes.zoom = newZoom / this._zoom;
+
+        this._zoom = newZoom;
+
+        this.updateRenderRadius();
+    }
+
+    get zoom(){
+        return this._zoom;
+    }
+
+    private updateRenderRadius(){
+        this._renderRadius = Math.hypot(this.width, this.height) / 2
+    }
+}
---
+++ src/CameraChanges.ts
@@ -0,0 +1,16 @@
+class CameraChanges {
+    x = 0;
+
+    y = 0;
+
+    zoom = 1;
+
+    direction = 0;
+
+    reset() {
+        this.x = 0;
+        this.y = 0;
+        this.zoom = 1;
+        this.direction = 0;
+    }
+}
