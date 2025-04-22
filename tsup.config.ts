82402fb103019dc49afee4bf4a8f7218357b947c
Sergey Zvyagintsev <nitron.ru@gmail.com>
2025-04-22 17:34:12 +0700

    Fix tsup build output file overwriting

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,27 +1,38 @@
 import { defineConfig } from 'tsup';
 
+const extensionMap = {
+    esm: 'mjs',
+    cjs: 'cjs',
+    iife: 'js'
+};
+
+const outExtension = ({format}) => ({
+    js: `.${extensionMap[format]}`
+});
+
+const commonConfig = {
+    outExtension,
+    publicDir: 'public',
+    clean: true,
+    minify: true,
+    sourcemap: true
+};
+
 export default defineConfig([
     {
+        ...commonConfig,
         entry: ['src/scrub.ts'],
-        format: ['esm', 'cjs'],
-        publicDir: 'public',
-        clean: true,
-        minify: true,
-        sourcemap: true
+        format: ['esm']
     },
     {
-        entry: ['src/browser/scrub.ts'],
-        format: ['iife'],
-        outExtension: () => ({
-            js: '.js'
-        }),
-        publicDir: 'public',
-        clean: true,
-        minify: true,
-        sourcemap: true
-    },
-    {
+        ...commonConfig,
         entry: ['src/scrub.ts'],
+        format: ['cjs'],
         dts: true
+    },
+    {
+        ...commonConfig,
+        entry: ['src/browser/scrub.ts'],
+        format: ['iife']
     }
 ]);
